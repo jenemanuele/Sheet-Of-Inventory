9ae14fe48c73cdf0e0a446923630ddc872460798
Jennifer Emanuele <jemanuele@gmail.com>
2022-02-09 20:23:35 -0500

    db seeds

---
+++ seeds/acoustic-seeds.js
@@ -0,0 +1,42 @@
+// const { Acoustic } = require('../models');
+
+const acousticGuitars = [
+    {
+        // guitar_id: 6,
+        brand: 'Martin',
+        name: 'D28 Acoustic',
+        price: 2999,
+        color: 'Sunburst',
+        strings: 6,
+    },
+    {
+        brand: 'Martin',
+        name: 'LX1 Little Martin',
+        price: 339,
+        color: 'Natural',
+        strings: 6
+    },
+    {
+        brand: 'Taylor',
+        name: 'GS Mini-e Koa Plus',
+        price: 1099,
+        color: 'Shaded Edgeburst',
+        strings: 6
+    },
+    {
+        brand: 'Taylor',
+        name: 'Baby Mahogany BT2',
+        price: 399,
+        color: 'Mahogany',
+        strings: 6
+    },
+    {
+        brand: 'Guild',
+        name: 'D-1212',
+        price: 779,
+        color: 'Natural',
+        strings: 12
+    }
+];
+
+module.exports = seedAcoustic;
