b66b681a2674304ee867a5032383d7799f40117f
Your Name <you@example.com>
2022-02-12 13:36:26 -0500

    got server working

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+// Routes: user, instrument(i.e. guitar), category(i.e. eletric), brand(i.e. Fender), item (the specific guitar)
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+const productRoutes = require('./product-routes');
+const categoryRoutes = require('./category-routes');
+
+router.use('/user', userRoutes);
+router.use('/category', categoryRoutes);
+router.use('/product', productRoutes);
+
+module.exports = router;
