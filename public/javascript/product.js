0042247482c0735173258ac134e37237942dc08b
emilypier <piersoee@miamioh.edu>
2022-02-15 22:56:27 -0500

    added values for varialbes in product.js

--- public/javascript/product.js
+++ public/javascript/product.js
@@ -2,12 +2,18 @@ async function productSubmit(event) {
   event.preventDefault();
 
   console.log("product test");
-  //add the values for these variables.
+  //add the values for these variables. hardcoded example below to show it works
   const product_name = "gibson les paul";
   const brand = "gibson";
-  const category_id = 1;
   const cost = 499.99;
   const body_type = "solid";
+  const category_id = 1;
+
+  // const product_name = document.querySelector('input[id="nameInput"]').value;
+  // const brand = document.querySelector('input[id="brandInput"]').value;
+  // const cost = document.querySelector('input[id="costInput"]').value;
+  // const body_type = document.querySelector('input[id="bodytypeInput"').value;
+  // const category_id = document.querySelector('input[id="categoryInput"]').value;
 
   await fetch("/api/product", {
     method: "POST",
