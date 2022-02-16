5a594da92b4a0afc22692b70f1141aab2a6d61ca
emilypier <piersoee@miamioh.edu>
2022-02-16 11:57:49 -0500

    created README

--- public/javascript/product.js
+++ public/javascript/product.js
@@ -3,17 +3,17 @@ async function productSubmit(event) {
 
   console.log("product test");
   //add the values for these variables. hardcoded example below to show that it works
-  const product_name = "gibson les paul";
-  const brand = "gibson";
-  const cost = 499.99;
-  const body_type = "solid";
-  const category_id = 1;
+  // const product_name = "gibson les paul";
+  // const brand = "gibson";
+  // const cost = 499.99;
+  // const body_type = "solid";
+  // const category_id = 1;
 
-  // const product_name = document.querySelector('input[id="nameInput"]').value;
-  // const brand = document.querySelector('input[id="brandInput"]').value;
-  // const cost = document.querySelector('input[id="costInput"]').value;
-  // const body_type = document.querySelector('input[id="bodytypeInput"').value;
-  // const category_id = document.querySelector('input[name="categoryInput"]').value;
+  const product_name = document.querySelector('input[id="nameInput"]').value;
+  const brand = document.querySelector('input[name="brandInput"]').value;
+  const cost = document.querySelector('input[id="costInput"]').value;
+  const body_type = document.querySelector('input[id="bodyTypeInput"').value;
+  const category_id = document.querySelector('input[name="categoryInput"]').value;
 
   await fetch("/api/product", {
     method: "POST",
