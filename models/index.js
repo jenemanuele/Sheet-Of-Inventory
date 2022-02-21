0490cb155c23f271ba8dc12dc5f0685a635cae65
emilypier <piersoee@miamioh.edu>
2022-02-20 20:16:53 -0500

    adding heroku

--- models/index.js
+++ models/index.js
@@ -4,42 +4,15 @@ const Product = require('./Product');
 const Category = require('./Category');
 
 // Create associations between the models
-// User-Category relationship
-User.hasMany(Category, {
-  foreignKey: 'user_id'
-});
-//Category-User relationship
-Category.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-// Product-User relationship
 Product.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'cascade',
-  hooks:true
+  onDelete: 'CASCADE'
 });
 
-// Product-Category relationship
 Product.belongsTo(Category, {
-  foreignKey: 'Category_id',
-  onDelete: 'cascade',
-  hooks: true
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
-
-// User-Product relationsihp
-User.hasMany(Product, {
-  foreignKey: 'user_id',
-  onDelete: 'cascade',
-  hooks:true
-});
-
-// Category-Product relationship
-Product.hasMany(Product, {
-  foreignKey: 'product_name',
-  onDelete: 'cascade',
-  hooks:true
-})
 
 
 module.exports = { User, Category, Product };
